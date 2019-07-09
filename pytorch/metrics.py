486ebde221524fefd2cbc35bf674b06165ec6d42
tmac1997 <89370436@qq.com>
2019-07-09 19:44:38 +0800

    change summary function and add mixup

--- pytorch/metrics.py
+++ pytorch/metrics.py
@@ -34,86 +34,42 @@ def F_score(logit, label, threshold=0.5, beta=2):
     F2 = (1 + beta**2) * precision * recall / (beta**2 * precision + recall + 1e-12)
     return F2.mean(0)
 
-
 class FocalLoss(nn.Module):
-    """
-    This is a implementation of Focal Loss with smooth label cross entropy supported which is proposed in
-    'Focal Loss for Dense Object Detection. (https://arxiv.org/abs/1708.02002)'
-        Focal_Loss= -1*alpha*(1-pt)*log(pt)
-    :param num_class:
-    :param alpha: (tensor) 3D or 4D the scalar factor for this criterion
-    :param gamma: (float,double) gamma > 0 reduces the relative loss for well-classified examples (p>0.5) putting more
-                    focus on hard misclassified example
-    :param smooth: (float,double) smooth value when cross entropy
-    :param balance_index: (int) balance class index, should be specific when alpha is float
-    :param size_average: (bool, optional) By default, the losses are averaged over each loss element in the batch.
-    """
-
-    def __init__(self, num_class, alpha=None, gamma=2, balance_index=-1, smooth=None, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.num_class = num_class
-        self.alpha = alpha
+    def __init__(self, gamma=2):
+        super().__init__()
         self.gamma = gamma
-        self.smooth = smooth
-        self.size_average = size_average
-
-        if self.alpha is None:
-            self.alpha = torch.ones(self.num_class, 1)
-        elif isinstance(self.alpha, (list, np.ndarray)):
-            assert len(self.alpha) == self.num_class
-            self.alpha = torch.FloatTensor(alpha).view(self.num_class, 1)
-            self.alpha = self.alpha / self.alpha.sum()
-        elif isinstance(self.alpha, float):
-            alpha = torch.ones(self.num_class, 1)
-            alpha = alpha * (1 - self.alpha)
-            alpha[balance_index] = self.alpha
-            self.alpha = alpha
-        else:
-            raise TypeError('Not support alpha type')
-
-        if self.smooth is not None:
-            if self.smooth < 0 or self.smooth > 1.0:
-                raise ValueError('smooth value should be in [0,1]')
 
     def forward(self, input, target):
-        logit = F.softmax(input, dim=1)
+        if not (target.size() == input.size()):
+            raise ValueError("Target size ({}) must be the same as input size ({})"
+                             .format(target.size(), input.size()))
 
-        if logit.dim() > 2:
-            # N,C,d1,d2 -> N,C,m (m=d1*d2*...)
-            logit = logit.view(logit.size(0), logit.size(1), -1)
-            logit = logit.permute(0, 2, 1).contiguous()
-            logit = logit.view(-1, logit.size(-1))
-        target = target.view(-1, 1)
+        max_val = (-input).clamp(min=0)
+        loss = input - input * target + max_val + \
+               ((-max_val).exp() + (-input - max_val).exp()).log()
 
-        # N = input.size(0)
-        # alpha = torch.ones(N, self.num_class)
-        # alpha = alpha * (1 - self.alpha)
-        # alpha = alpha.scatter_(1, target.long(), self.alpha)
-        epsilon = 1e-10
-        alpha = self.alpha
-        if alpha.device != input.device:
-            alpha = alpha.to(input.device)
+        invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
+        loss = (invprobs * self.gamma).exp() * loss
 
-        idx = target.cpu().long()
+        return loss.sum(dim=1).mean()
+    
+class FbetaLoss(nn.Module):
+    def __init__(self, beta=1):
+        super(FbetaLoss, self).__init__()
+        self.small_value = 1e-6
+        self.beta = beta
 
-        one_hot_key = torch.FloatTensor(target.size(0), self.num_class).zero_()
-        one_hot_key = one_hot_key.scatter_(1, idx, 1)
-        if one_hot_key.device != logit.device:
-            one_hot_key = one_hot_key.to(logit.device)
+    def forward(self, logits, labels):
+        beta = self.beta
+        batch_size = logits.size()[0]
+        p = F.sigmoid(logits)
+        l = labels
+        num_pos = torch.sum(p, 1) + self.small_value
+        num_pos_hat = torch.sum(l, 1) + self.small_value
+        tp = torch.sum(l * p, 1)
+        precise = tp / num_pos
+        recall = tp / num_pos_hat
+        fs = (1 + beta * beta) * precise * recall / (beta * beta * precise + recall + self.small_value)
+        loss = fs.sum() / batch_size
+        return 1 - loss
 
-        if self.smooth:
-            one_hot_key = torch.clamp(
-                one_hot_key, self.smooth, 1.0 - self.smooth)
-        pt = (one_hot_key * logit).sum(1) + epsilon
-        logpt = pt.log()
-
-        gamma = self.gamma
-
-        alpha = alpha[idx]
-        loss = -1 * alpha * torch.pow((1 - pt), gamma) * logpt
-
-        if self.size_average:
-            loss = loss.mean()
-        else:
-            loss = loss.sum()
-        return loss    
